c4c272c67a60cf67bc998adfcf55efb8b5a8f1a2
Robert Garrigos <robert@garrigos.cat>
2024-11-09 11:53:14 +0100

    fixet html and classes

--- iivblock-content.tpl.php
+++ iivblock-content.tpl.php
@@ -13,8 +13,8 @@
  * @see template_preprocess_block()
  */
 ?>
-<div class="row <?php print $wrapper_classes; ?>">
-  <?php if ($image_position == 'left' || $image_position == 'right'): ?>
+<?php if ($image_position == 'left' || $image_position == 'right'): ?>
+  <div class="row <?php print $wrapper_classes; ?>">
     <?php if ($image_position == 'left'): ?>
       <div class="col-md-6 iivblock-image first <?php print $image_transition; ?>">
         <?php print $image ?>
@@ -30,32 +30,39 @@
         <?php print $image ?>
       </div>
     <?php endif; ?>
-  <?php else: ?>
-    <?php if ($image_position == 'up'): ?>
-      <div class="row">
-        <div class="col-md-6 iivblock-image first <?php print $image_transition; ?>">
-          <?php print $image ?>
-        </div>
+    <script>
+      iivblock_observer.observe(document.querySelector(".iivblock-inner.<?php print $machine_name; ?>"));
+    </script>
+  </div>
+<?php else: ?>
+  <div class="row <?php print $wrapper_classes; ?>">
+    <div class="col">
+      <?php if ($image_position == 'up'): ?>
         <div class="row">
-          <div class="col-md-6 iivblock-body second  <?php print $content_transition; ?>">
-            <?php print $content ?>
-          </div>
-        </div>
-      </div>
-    <?php else: ?>
-      <div class="row">
-        <div class="col-md-6 iivblock-body first <?php print $content_transition; ?>">
-          <?php print $content ?>
-        </div>
-        <div class="row">
-          <div class="col-md-6 iivblock-image second  <?php print $image_transition; ?>">
+          <div class="col d-flex justify-content-center iivblock-image first <?php print $image_transition; ?>">
             <?php print $image ?>
           </div>
         </div>
-      </div>
-    <?php endif; ?>
-  <?php endif; ?>
-  <script>
-    iivblock_observer.observe(document.querySelector(".iivblock-inner.<?php print $machine_name; ?>"));
-  </script>
-</div>
+        <div class="row">
+          <div class="col d-flex justify-content-center iivblock-body second  <?php print $content_transition; ?>">
+            <?php print $content ?>
+          </div>
+        </div>
+      <?php else: ?>
+        <div class="row">
+          <div class="col d-flex justify-content-center iivblock-body first <?php print $content_transition; ?>">
+            <?php print $content ?>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col d-flex justify-content-center iivblock-image second <?php print $image_transition; ?>">
+            <?php print $image ?>
+          </div>
+        </div>
+      <?php endif; ?>
+    </div>
+    <script>
+      iivblock_observer.observe(document.querySelector(".iivblock-inner.<?php print $machine_name; ?>"));
+    </script>
+  </div>
+<?php endif; ?>
